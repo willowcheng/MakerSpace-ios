5518a275cb2d16c133b00becf38085442f0fe9ab
willowcheng <e.wingus@Gmail.com>
2015-06-19 15:12:10 -0400

    Initial Commit

---
+++ MakerSpace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakerSpace
+//
+//  Created by Liu Cheng on 2015-06-19.
+//  Copyright (c) 2015 Makerspace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
